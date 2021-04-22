9190f626ba9c9e967b1c93082d751ad7c0a4d2a9
Henrik Gripenberg <henrik.gripenberg@finn-id.fi>
2021-04-22 18:33:45 +0300

    Added models

--- rock_paper_scissors/game/models.py
+++ rock_paper_scissors/game/models.py
@@ -1,6 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from enum import IntEnum
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+NUMBER_OF_CHOICES = 3
+
+
+class PlayerAction(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+
+
+class Score(models.Model):
+    user_choice = models.CharField(
+      max_length=NUMBER_OF_CHOICES,
+      choices=[(tag, tag.value) for tag in PlayerAction]
+    )
+    computer_choice = models.CharField(
+      max_length=NUMBER_OF_CHOICES,
+      choices=[(tag, tag.value) for tag in PlayerAction]
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return [self.user_choice,
+                self.computer_choice,
+                self.created]
+        
+
