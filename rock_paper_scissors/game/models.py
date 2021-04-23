1dbd872db1c99770a06adaa14ea8e39d45933a96
Henrik Gripenberg <32542377+grihenrik@users.noreply.github.com>
2021-04-23 19:22:30 +0300

    Update models.py

--- rock_paper_scissors/game/models.py
+++ rock_paper_scissors/game/models.py
@@ -5,13 +5,7 @@ from enum import IntEnum
 from django.db import models
 from django.conf import settings
 
-NUMBER_OF_CHOICES = 3
 
-
-class PlayerAction(IntEnum):
-    Rock = 2
-    Paper = 3
-    Scissors = 4
 
 class PlayerChoice(models.Model):
     user_choice = models.CharField(max_length=50)
