055a983b68ba31f502d4ed7578d412827305e2cd
Henrik Gripenberg <32542377+grihenrik@users.noreply.github.com>
2021-04-23 19:18:52 +0300

    Removed django default secret key

--- rock_paper_scissors/settings.py
+++ rock_paper_scissors/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j$4509remc(89&y0^vem9ji_#!6pxtq7^^^0*ydglj^k4)*a&g'
+SECRET_KEY = 'add_a_secret_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
